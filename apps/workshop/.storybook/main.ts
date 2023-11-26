8427d11b96564c664210f83902b3f82592760425
Ivan Avila <ivelaval@gmail.com>
2023-11-26 11:55:41 -0500

    Some upgrades were added

--- apps/workshop/.storybook/main.ts
+++ apps/workshop/.storybook/main.ts
@@ -1,9 +1,10 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-mdx-gfm'],
+  addons: [getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials"), getAbsolutePath("@storybook/addon-interactions"), getAbsolutePath("@storybook/addon-mdx-gfm")],
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {}
   },
   docs: {
@@ -11,3 +12,7 @@ const config: StorybookConfig = {
   }
 };
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
